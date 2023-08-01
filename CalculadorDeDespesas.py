a696e8d2f50cef1788fd74c46653233ab5c5a25d
Levi2204 <leviteixeira225@gmail.com>
2023-08-01 14:43:22 -0300

    segundo commit

--- CalculadorDeDespesas.py
+++ CalculadorDeDespesas.py
@@ -1,14 +1,18 @@
 print('CALCULADORA DE DESPESAS')
 bancos = int(input('Quantos bancos você está com a fatura aberta?:'))
+print('-'*20)
 faturaTotal = []
 guardadoTotal = []
 for contador in range (bancos):
     fatura = float(input('Digite quanto está sua fatura do banco {}: '.format(contador + 1)))
     faturaTotal.append(fatura)
+print('-'*20)
 print('Você tem algum dinheiro guardado?\n1 = sim\n2 = não')
 escolha = int(input(''))
+print('-'*20)
 if escolha == 1:
     QuantidadeGuardado = int(input('Em quantos locais você tem dinheiro guardado?:'))
+    print('-' * 20)
     for contadorDois in range(QuantidadeGuardado):
         guardado = float(input('Digite quanto está guardado no {}° local: '.format(contadorDois + 1)))
         guardadoTotal.append(guardado)
@@ -18,7 +22,7 @@ else:
     print('Opção invalida')
 
 resultado = sum(faturaTotal) - sum(guardadoTotal)
-
+print('-'*20)
 if resultado <= 0:
     print('Você tem nenhuma divida e tem {} reais sobrando'.format(resultado))
 elif resultado > 0:
